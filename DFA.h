77f63c890c5fd4e3ed04b101f42578d84222650c
Cuzeth <59286294+Cuzeth@users.noreply.github.com>
2025-02-13 18:38:26 -0700

    Update DFA.h

--- DFA.h
+++ DFA.h
@@ -3,6 +3,7 @@
 
 #include <set>
 #include <map>
+#include <string>
 
 using namespace std;
 
@@ -18,25 +19,25 @@ public:
     DFA(set<char> A, set<int> I, set<int> F);
     void Reset();
 
-    void AddTransition( int src, int dst, char sym) {Dtran[src][sym] = dst;}
-    void Move( char c);
-    DFAstatus GetStatus() {return status;}
-    string GetAcceptedLexeme() {return accepted_lexeme;}
-    bool GetAccepted() {return accepted;}
+    void AddTransition(int src, int dst, char sym) { Dtran[src][sym] = dst; }
+    void Move(char c);
+    bool Run(const std::string& input);
+    DFAstatus GetStatus() { return status; }
+    string GetAcceptedLexeme() { return accepted_lexeme; }
+    bool GetAccepted() { return accepted; }
     void Print();
 
 private:
-    map< int, map<char, int> > Dtran;
+    std::map<int, std::map<char, int>> Dtran;
     DFAstatus status;
     int current_state;
     bool accepted;
-    string lexeme;
-    string accepted_lexeme;
-    
-    set<char> alpha;      // set of input symbols in the alphabet
-    char epsilon = '_';   // epsilon is represented by '_'
-    set<int> init_states; // initial state of the DFA
-    set<int> fin_states;  // final states of the DFA
+    std::string lexeme;
+    std::string accepted_lexeme;
+
+    std::set<char> alpha;      // set of input symbols in the alphabet
+    std::set<int> init_states; // initial state of the DFA
+    std::set<int> fin_states;  // final states of the DFA
 };
 
 #endif
